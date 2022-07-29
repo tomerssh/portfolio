ae07dbb6ec18ee92284007eb4f6da77601308551
Tomer <37521314+tomerssh@users.noreply.github.com>
2022-07-29 18:10:38 +0300

    update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,21 +1,30 @@
 export const projects = [
   {
     title: 'Coupon System',
-    description: 'Fullstack SPA built using java spring, spring data jpa and mysql for the backend, and angular, angular material for the frontend',
+    description: 'Fullstack SPA Built using Java Spring, Spring Data JPA and MySQL for the Backend, And Angular, Angular Material for the Frontend',
     image: '/images/coupons.png',
-    tags: ['Spring', 'Angular'],
+    tags: ['Spring', 'Angular', 'Angular Material'],
     code: 'https://github.com/tomerssh/CouponSystem',
     visit: 'https://coupon-system-new.herokuapp.com',
     id: 0,
   },
   {
     title: 'EShop',
-    description: 'E-Commerce web application built using React, Commerce.js and Stripe for payment integration',
+    description: 'E-Commerce Web Application built using React, Material UI, Commerce.js and Stripe for Payment Integration',
     image: '/images/eshop.png',
     tags: ['React', 'Commerce.js', 'Stripe'],
     code: 'https://github.com/tomerssh/eshop',
     visit: 'https://tomereshopproject.netlify.app/',
     id: 1,
+  },
+  {
+    title: 'Travel Advisor',
+    description: 'Travel Companion Web Application Built using React, Material UI, Google Maps API, and RapidAPI\'s Travel Advisor API',
+    image: '/images/traveladvisor.png',
+    tags: ['React', 'Google Maps API', 'RapidAPI'],
+    code: 'https://github.com/tomerssh/travel-advisor',
+    visit: 'https://tomertraveladvisorproject.netlify.app/',
+    id: 2,
   },
 ];
 
